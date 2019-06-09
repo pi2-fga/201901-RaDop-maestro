1420e17b4a7099c148a302aab33d0e20e6145c39
diego-bf <diegobmfranca@gmail.com>
2019-06-09 18:42:48 -0300

    Code organization

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import consumer.queue_worker as worker
 import caller.triage as triage
 import caller.selector as selector
 
+
 def start_symphony(body_msg):
     dict_msg = triage.transform_json_payload(body_msg)
 
