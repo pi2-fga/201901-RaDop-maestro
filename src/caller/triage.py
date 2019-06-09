99b6b5e89a975092db265dbdcdcf579e685541e5
diego-bf <diegobmfranca@gmail.com>
2019-06-09 18:36:59 -0300

    Add new triage methods
    - Add a method to find the best candidate plate
    - Add a method to check if a base 64 string is valid

--- src/caller/triage.py
+++ src/caller/triage.py
@@ -1,5 +1,5 @@
 import json
-import time
+import re
 
 # take the json payload from the message body and transform in a dictionary
 def transform_json_payload(message_body):
@@ -13,3 +13,29 @@ def transform_json_payload(message_body):
     print(f'generated dictionary: {dict_payload}')
     return dict_payload
 
+
+# extract the best candidate to a vehicle's plate
+def extract_plate(dict_json):
+    if dict_json:
+        candidates_alpr = dict_json['response']['results'][0]['candidates']
+        plate = None
+        best_confidence = -1
+
+        for candidate in candidates_alpr:
+            if candidate['matches_template'] == 1 and candidate['confidence'] > best_confidence:
+                plate = candidate['plate']
+                best_confidence = candidate['confidence']
+
+        print(f'The best plate id is: {plate}')
+        return plate
+    else:
+        return None
+
+
+def valid_base64(str_base64):
+    if str_base64:
+        pattern = re.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$")
+        return bool(pattern.match(str_base64))
+
+    else:
+        return False
