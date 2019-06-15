f17d5b72fbae6ba27857ad76dbccee108f17686d
diego-bf <diegobmfranca@gmail.com>
2019-06-15 13:31:44 -0300

    Change the log's messages format

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import consumer.queue_worker as worker
+# import consumer.simple_consume_worker as simple_worker
 import caller.triage as triage
 import caller.selector as selector
+import logging
 
+LOG_FORMAT = ('%(asctime)s %(levelname)10s - %(name)s %(funcName)s:\n%(message)s')
+LOGGER = logging.getLogger(__name__)
 
 def start_symphony(body_msg):
+    LOGGER.info('Starting the symphony')
     dict_msg = triage.transform_json_payload(body_msg)
 
     selector.select_action(dict_msg)
